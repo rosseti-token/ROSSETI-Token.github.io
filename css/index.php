330a5422fe0a9b1228f7746f58bb1423960d2f11
srsjes65 <76631735+srsjes65@users.noreply.github.com>
2021-12-30 12:51:50 +0800

    Add files via upload

---
+++ css/index.php
@@ -0,0 +1,293 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>ROSSETI Token/coin BNB</title>
+<meta charset="utf-8">
+<meta name="description" content="ROSSETI">
+<meta name="keywords" content="ROSSETI TOKENS, debugger, windows, linux, bsd, open source, nekotekina, home">
+<meta name="author" content="ROSSETI TOKENS">
+<meta name="copyright" content="ROSSETI TOKENS">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+</head>
+<body>
+<img src="/img/graphics/landing/hero-main.png" width="1" height="1" alt="preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/inc-video-popout.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'PU3A755xcDQ',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'57L0OL85ILU',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'pNmLjPpw5xY',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>ROSSETI<p> token/coin<br>
+							  is a cryptocurrency, deployed on Binance Smart Chain (BSC BEP-20) </h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 ROSSETI TOKENS<p>
+                                 Contract:  0x88bcb2a9c54818b967c1c3604633de21b920e227
+							</p>
+							<div class='landing-btn-container'>
+								<div class="build-btn-button">
+									<div class="build-ico-button">
+										<div class="build-ico-os">
+											<img src="img/icons/os/windows.png" style='height: 100%; width: 100%; object-fit: contain'/>
+											<img src="img/icons/os/linux.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+											<img src="img/icons/os/bsd.png" style='height: 110%; width: 110%; object-fit: contain; display:none;'/>
+										</div>
+									</div>
+									<a href="https://bscscan.com/token/0x88bcb2a9c54818b967c1c3604633de21b920e227" target="_blank">
+										<div class="build-tx1-button">
+										<span>Enter</span>
+										<span class="build-tx2-label">BscScan</span>
+									</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/hero-main.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-search darkmode-slimbar">
+		<div class="search-ovr-container">
+		</div>
+		<div class='search-con-container'>
+			<div class="search-con-outer">
+				<div class="search-con-inner">
+					<div class="search-inp-search darkmode-search-border">
+						<div class="search-ico-search darkmode-invert">
+						</div>
+						<div class="search-con-author darkmode-txt">
+							<a href="https://bscscan.com/token/0x88bcb2a9c54818b967c1c3604633de21b920e227" target="_blank">
+							<span class="darkmode-txt">Compatibility DB maintained by <span class="search-img-author"></span>
+							<span class="search-tx1-author">ROSSETI</span>
+							</span>
+							</a>
+						</div>
+						<form action='/compatibility' method='get'>
+							<input class="database-search darkmode-txt darkmode-search-bg" name="g" placeholder="GitHub / ROSSETI ID">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-video darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-btn-left darkmode-arrow-left left" href="#myCarousel" data-slide="prev">
+			</div>
+			<div class="content-btn-right darkmode-arrow-right right" href="#myCarousel" data-slide="next">
+			</div>
+			<div class="content-con-inside">
+				<div class="video-tx1-heading darkmode-txt">
+					<h2>ROSSETI news</h2>
+				</div>
+				<div class="video-con-divider">
+				</div>
+				<?php include 'lib/module/inc-video-carousel.php';?>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-discord darkmode-discord">
+		<div class="discord-left-logo">
+		     <img src="/img/graphics/svg/discord-left.svg" style="width: 150%; height: 150%;">
+		        </div>
+		<div class="discord-center-logo">
+			<img src="/img/graphics/svg/discord-center.svg" style="width: 50%; height: 50%;">
+		</div>
+		<div class="discord-right-logo">
+			<img src="/img/graphics/svg/discord-right.svg" style="width: 80%; height: 80%;">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-img-logo">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>Trust Wallet</h2>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									 Trust Wallet надёжный Кошелёк для токена ROSSETI – это анонимный децентрализованный кошелек для хранения и использования криптовалюты. Ключевой особенностью кошелька является: полная анонимность, высокая надежность (ключи от кошелька хранятся у пользователя + за все время работы в кошельке не было проблем с безопасностью и кражей средств пользователей), простота использования, поддержка многих криптовалют, есть поддержка DApp приложений, обмен и торговля криптовалютой прямо в кошельке. С 2018 года крипто кошельком Trust Wallet владеет крупнейшая биржа криптовалют Binance
+								</p>
+								</div>
+							<a href="https://trustwallet.com/ru/" target="_blank">
+							<div class="discord-btn-button">
+								<div class="discord-ico-button">
+								</div>
+								<div class="discord-tx1-button">
+									<span>Trust Wallet</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.1" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+			<span>Advertisement</span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-github">
+		<div class="github-img-backdrop">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>MetaMask</h2>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									MetaMask — это безопасное приложение для криптовалютного кошелька, предназначенное для хранения, управления, получения и передачи токенов ROSSETI из Binance Smart Chain. MetaMask очень безопасен, у него миллионы активных пользователей, и он неуклонно растет с лояльными последователями по всему миру. С MetaMask вы владеете ключами к своему криптовалютному кошельку, никакая третья сторона не задействована. Он быстро и легко интегрируется с браузерами, обеспечивая в целом отличный пользовательский интерфейс с минимальными хлопотами при покупке, отправке или получении криптовалюты.
+								</p>
+							</div>
+							<a href="https://metamask.io/" target="_blank">
+							<div class="github-btn-button">
+								<div class="github-ico-button">
+								</div>
+								<div class="github-tx1-button">
+									<span>MetaMask</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+			<span>Advertisement</span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="patreon-img-backdrop">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra">
+								<h2>ROSSETI-PancakeSwap</h2>
+								<div class="landing-con-divider" style="background: #c8a2c8">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									 PancakeSwap – крупнейшая по объему торгов децентрализованная биржа для сети Binance Smart Chain (BSC), это автоматизированный маркет-мейкер (AMM) — приложение децентрализованных финансов (DeFi), которое позволяет пользователям обменивать токены ROSSETI и другие, обеспечивая ликвидность через доходное фермерство и комиссионные платежи. PancakeSwap – яркий представитель нового поколения криптовалютных бирж, которые работают децентрализовано. Платформа уже успела занять заметную позицию на рынке и имеет суточный оборот, достигающий 500$ млн.
+									 </p>Перейти на биржу
+								</p>
+							</div>
+							<a href="https://pancakeswap.finance/info/token/0x88BCb2a9C54818b967c1c3604633De21B920e227" target="_blank">
+							<div class="patreon-btn-button">
+								<div class="patreon-ico-button">
+								</div>
+								<div class="patreon-tx1-button">
+									<span>PancakeSwap</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
