34f396a1c988eec9c6573aae81a675dc2aee8b9f
srsjes65 <76631735+srsjes65@users.noreply.github.com>
2021-12-30 14:51:58 +0800

    Add files via upload

---
+++ alipay.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>ROSSETI - Alipay</title>
+<meta charset="utf-8">
+<meta name="description" content="ROSSETI emulator and debugger written in C++ for Windows, Linux and BSD.">
+<meta name="keywords" content="ROSSETI, emulator, debugger, windows, linux, bsd, open source, nekotekina, kd11, alipay">
+<meta name="author" content="ROSSETI">
+<meta name="copyright" content="ROSSETI">
+<meta name="robots" content="noindex, nofollow">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-backdrop darkmode-header" style="position:fixed;">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='alipay-con-alipay'>
+			<div class="alipay-ico-logo">
+			</div>
+			<a href='/img/icons/qr/alipay.png' target="_blank">
+			<div class="alipay-ico-alipay">
+			</div>
+			</a>
+			<div class="alipay-tx1-prompt pulsate">
+				<h1>ROSSETI</h1>
+			</div>
+			<div class="alipay-tx2-prompt pulsate">
+				<span>ROSSETI Token</span>
+			</div>
+		</div>
+		<div class="alipay-con-footer">
+			<p>
+				 © 2021 ROSSETI VK.
+			</p>
+		</div>
+	</div>
+</div>
+</body>
+</html>
