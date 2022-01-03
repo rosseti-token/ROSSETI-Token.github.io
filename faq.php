eeefe470deec4ea4190a1a03d67278628239c107
rosseti-token <76631735+rosseti-token@users.noreply.github.com>
2022-01-03 08:16:11 +0800

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,248 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>ROSSETI - FAQs</title>
+<meta charset="utf-8">
+<meta name="description" content="ROSSETI.">
+<meta name="keywords" content="ROSSETI">
+<meta name="author" content="ROSSETI">
+<meta name="copyright" content="ROSSETI">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>FAQs</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Frequently asked questions
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class="landing-con-panel panel-fadeUp" style="background: url('/img/graphics/panels/faq.jpg') no-repeat center;">
+				<div class='landing-ovr-panel'>
+					<div class='landing-tx1-panel'>
+						<h2>One Step Ahead</h2>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>What is ROSSETI licensed under? </h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 ROSSETI uses the GNU General Public License Version 2 (June 1991). According to the license, you are welcome to use ROSSETI and its source code for any purpose, but distributing ROSSETI requires that the source code be released and attribution given. For more details on how the GNU General Public License system works, please refer to <a href="https://www.gnu.org/">GNU.org</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>How can I get started using ROSSETI on my PC?</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 Please refer to the <a href="/quickstart">Quickstart Guide</a> for detailed information on how to get started.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+					
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+					
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+					
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						
+						</p>
+						<br>
+						<p>
+							 
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+					
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+					
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+					
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+					
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+					
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+					
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+					
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						
+						</p>
+						<br>
+						
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include 'lib/module/inc-footer.php';?>
+</body>
+</html>
